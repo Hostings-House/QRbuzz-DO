1d73f72a8e7f2e8443cade9e579de847060e8a29
arfan-hostingshouse <50193175+arfan-hostingshouse@users.noreply.github.com>
2020-12-18 20:57:54 +0500

    Revert "Merge branch 'main' of https://github.com/Hostings-House/QRbuzz-DO into main"
    This reverts commit b99668f1d3bdde91d17f80874287dce59c69afaa, reversing
    changes made to c08006a15c0c128fe97416794a7b33815a666c82.

--- src/_metronic/_partials/dashboards/Demo2Dashboard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function Demo2Dashboard() {
-    return <>
-    <h2>Test Dashbord</h2>
-    </>;
-}
--- src/app/pages/Business/config.js
+++
@@ -1,5 +0,0 @@
-import {API_URL} from "../../Constants";
-
-export const API_URL = process.env.NODE_ENV === 'production'
-  ? API_URL + 'server/'
-  : 'http://localhost:3000'
